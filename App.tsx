48e2cafd8c636f9a44256cddb86a97505aa709aa
Aurélien Leygues <aurelien.leygues@gmail.com>
2023-05-24 16:25:27 +0200

    adding push notifications

--- App.tsx
+++ App.tsx
@@ -10,6 +10,50 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { UserProvider } from "./hooks/user.context";
 import Router from "./Router";
 import { NavigationContainer } from "@react-navigation/native";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import { Platform } from "react-native";
+import { useEffect, useRef, useState } from "react";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice || 1 + 1 == 2) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
+}
 
 const httpLink = createHttpLink({
   uri: "http://10.0.2.2:5000",
@@ -33,6 +77,26 @@ const client = new ApolloClient({
 });
 
 function App() {
+  const [expoPushToken, setExpoPushToken] = useState("");
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    Notifications.addNotificationReceivedListener((notification) => {
+      console.log("Notification");
+      console.log(JSON.stringify(notification, null, 4));
+      // navigate('Post', { id: ... })
+    });
+
+    Notifications.addNotificationResponseReceivedListener((response) => {
+      console.log("Response");
+      console.log(JSON.stringify(response, null, 4));
+      // navigate('Post', { id: ... })
+    });
+  }, []);
+
   return (
     <ApolloProvider client={client}>
       <NavigationContainer>
